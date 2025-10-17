5a152fbe5b2da71e47f3c30456cb7d555114f14a
Mohammad Alomar <mohammedahalomar@gmail.com>
2025-10-17 13:52:50 +0300

    Adding urls files.

--- darek_web/listings/views.py
+++ darek_web/listings/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def showListings(request):
+    return HttpResponse("Hello from listings!")
 # Create your views here.
--- darek_web/users/views.py
+++ darek_web/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+urlpatterns = [
+    path('',views.showListings,name="listings")
+    ]
